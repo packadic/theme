71f85d9262de68d6a5d9a7fe25e4b78ef9b95873
Robin Radic <robin@radic.nl>
2015-06-29 05:05:54 +0200

    timeline added, recent comments added.

--- src/scripts/demo/dashboard.js
+++ src/scripts/demo/dashboard.js
@@ -9,7 +9,7 @@ require([
 
     $(function () {
 
-        var initCharts = function () {
+        var PieCharts = function () {
             return;
             var charts = $('.percentage');
             var barColors = ['amber-dark', 'green', 'teal', 'cyan'];
@@ -21,8 +21,8 @@ require([
                         this.$el.find('span').text(~ ~ value);
                     },
                     barColor: theme.colors[barColors[i]],
-                    size: 85
-                }).css({ 'margin': '0px 15px' });
+                    size    : 85
+                }).css({'margin': '0px 15px'});
             });
             charts.on('click', function (e) {
                 e.preventDefault();
@@ -32,6 +32,85 @@ require([
             });
         }.call();
 
+
+        (function RealTimeMonitor() {
+            if ( ! jQuery.plot ) {
+                return;
+            }
+
+            var data = [];
+            var totalPoints = 250;
+
+            // random data generator for plot charts
+
+            function getRandomData() {
+                if ( data.length > 0 ) data = data.slice(1);
+                // do a random walk
+                while (data.length < totalPoints) {
+                    var prev = data.length > 0 ? data[data.length - 1] : 50;
+                    var y = prev + Math.random() * 10 - 5;
+                    if ( y < 0 ) y = 0;
+                    if ( y > 100 ) y = 100;
+                    data.push(y);
+                }
+                // zip the generated y values with the x values
+                var res = [];
+                for (var i = 0; i < data.length; ++ i) {
+                    res.push([i, data[i]]);
+                }
+
+                return res;
+            }
+
+            if ( $('#dashboard-rtm').size() != 1 ) {
+                return;
+            }
+            //server load
+            var options = {
+                series: {
+                    shadowSize: 1
+                },
+                lines : {
+                    show     : true,
+                    lineWidth: 0.5,
+                    fill     : true,
+                    fillColor: {
+                        colors: [{
+                            opacity: 0.1
+                        }, {
+                            opacity: 1
+                        }]
+                    }
+                },
+                yaxis : {
+                    min          : 0,
+                    max          : 100,
+                    tickColor    : "#eee",
+                    tickFormatter: function (v) {
+                        return v + "%";
+                    }
+                },
+                xaxis : {
+                    show: false,
+                },
+                colors: [theme.colors['amber'], theme.colors['teal']],
+                grid  : {
+                    tickColor  : "#eee",
+                    borderWidth: 0,
+                }
+            };
+
+            var updateInterval = 30;
+            var plot = $.plot($("#dashboard-rtm"), [getRandomData()], options);
+
+            function update() {
+                plot.setData([getRandomData()]);
+                plot.draw();
+                setTimeout(update, updateInterval);
+            }
+
+            update();
+        }.call());
     });
 });
 
