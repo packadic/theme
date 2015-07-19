7518d64d60bbc55287a9cca48b7716e77959245a
Robin Radic <robin@radic.nl>
2015-07-19 09:26:55 +0200

    wizard is gonna require quite some time

--- src/tscripts/demo/forms-wizard.ts
+++ src/tscripts/demo/forms-wizard.ts
@@ -6,7 +6,6 @@ import packadic = require('packadic');
 import {defined,def,cre} from 'app/util';
 
 
-
 class FormsWizardDemo {
     private app:packadic.App;
 
@@ -16,8 +15,36 @@ class FormsWizardDemo {
 
     public init() {
         $(function () {
+            this.initWizard();
+        }.bind(this));
+    }
 
-        });
+    protected initWizard() {
+        var opts:WizardOptions = {
+            onNext: function (tab:any, navigation:any, index:number):boolean {
+                var $name:JQuery = $('#name');
+                if (index == 2) {
+                    // Make sure we entered the name
+                    if (!$name.val()) {
+                        alert('You must enter your name');
+                        $name.focus();
+                        return false;
+                    }
+                }
+
+                // Set the name for the next tab
+                $('#tab3').html('Hello, ' + $name.val());
+
+            },
+            onTabShow: function (tab:any, navigation:any, index:number):boolean {
+                var $total = navigation.find('li').length;
+                var $current = index + 1;
+                var $percent = ($current / $total) * 100;
+                $('#rootwizard').find('.bar').css({width: $percent + '%'});
+                return true;
+            }
+        };
+        $('#rootwizard').bootstrapWizard(opts);
     }
 }
 export = FormsWizardDemo
